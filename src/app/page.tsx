4cda9bc0e527fb1aa25ab49cc54f27d60ee78906
AhmadBahr <ahmad.bahr@mubs.edu.lb>
2024-12-23 19:13:04 +0200

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import { SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      <SignedOut>
+        <SignUpButton>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+        <UserButton/>
+      <SignedIn>
+        <SignOutButton>
+          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Out
+          </button>
+        </SignOutButton>
+      </SignedIn>
+    </div>
   )
 }
